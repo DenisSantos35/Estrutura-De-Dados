af1dfef13ed1f91128c8818b8b2b52fc95deb5b5
DenisSantos35 <denisdiogo18@hotmail.com>
2023-03-04 17:15:44 -0300

    Estrutura busca binatia

---
+++ EstruturadeDados/buscaBinaria.mjs
@@ -0,0 +1,23 @@
+//           0  1  2  3  4  5  6  7  8
+// let array = [11,22,33,44,55,66,77,88,99];
+// console.log(buscaSequencial(array,100))
+
+function buscaSequencial(array, valor){
+    let ini = 0;
+    let fim = array.length - 1
+    while(fim >= ini){
+        let meio = Math.floor((ini +fim) / 2);
+        if(valor === array[meio]){
+            return meio;
+        }else if(valor > array[meio]){
+            ini = meio + 1
+        }else{
+            fim = meio - 1
+        }        
+    }
+    return -1
+}
+
+import {nomes} from './data/vetor-nomes.mjs'
+
+console.log(buscaSequencial(nomes, 'JOYCE'));
